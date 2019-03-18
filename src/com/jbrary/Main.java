35138da11994ed8aa5281307124496a2bf1ce8ad
CyberSai <isaacsai030@gmail.com>
2019-03-18 12:48:23 +0000

    Refactoring project files

--- src/com/jbrary/Main.java
+++ src/com/jbrary/Main.java
@@ -7,12 +7,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private Scene welcome;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("welcome.fxml"));
+        primaryStage.setTitle("JBrary");
+        welcome = new Scene(root, 600, 400);
+        primaryStage.setScene(welcome);
         primaryStage.show();
     }
 
